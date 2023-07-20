55a546b2d2b6a851d9a91936a21f07a2f99cf8a4
lomick2090 <lomick2090@gmail.com>
2023-07-20 11:56:46 -0500

    Add nav

---
+++ frontend/src/app/about/index.tsx
@@ -0,0 +1,46 @@
+import Layout from "../components/Layout";
+
+export default function About() {
+    return (
+        <Layout>
+        <main className='p-20'>
+          <div className='text-[white]'>
+              <h3 className="text-lg font-bold ml-5">Welcome to CryptoSwapPro: Your Gateway to Universal Crosschain Swaps and Asset Security!</h3>
+  
+              Are you tired of being restricted to a single blockchain network for your cryptocurrency swaps? Say hello to CryptoSwapPro – the revolutionary crypto app that empowers you to conduct universal crosschain swaps while ensuring the utmost safety of your assets on the chain of your choice!
+  
+              Discover the Future of Crypto Swapping
+  
+              Gone are the days of limited options and high risks when swapping cryptocurrencies. CryptoSwapPro is here to revolutionize the way you trade your digital assets. Our cutting-edge technology allows you to seamlessly exchange coins and tokens across various blockchain networks. Whether you're an experienced trader or a newcomer to the world of crypto, our user-friendly interface ensures a smooth and effortless swapping experience.
+  
+            <h3 className='text-lg font-bold mt-5 ml-5'>Why Choose CryptoSwapPro?</h3>
+  
+            <ul>
+  
+              <li><span className="font-bold">1.</span> Universal Crosschain Swaps: Our platform is designed to break the barriers of traditional crypto exchanges. Unlock a vast universe of possibilities by conducting swaps across different blockchains, opening the door to countless trading opportunities.</li>
+  
+              <li><span className="font-bold">2.</span> Maximum Asset Security: We understand the value you place on your digital assets. With CryptoSwapPro, you're in complete control. Your assets stay safe on the chain of your choice throughout the swapping process, eliminating any security concerns.</li>
+  
+              <li><span className="font-bold">3.</span> Lightning-Fast Transactions: Time is money, and we value yours. Experience rapid transactions with our advanced blockchain technology, ensuring you never miss out on a trading opportunity due to sluggish networks.</li>
+  
+              <li><span className="font-bold">4.</span> User-Friendly Interface: No more navigating through complex systems. Our intuitive interface is tailored to cater to users of all levels, making crypto swapping accessible to everyone.</li>
+  
+              <li><span className="font-bold">5.</span> Transparent and Trustworthy: At CryptoSwapPro, transparency is our foundation. Rest assured, you'll always have full visibility of your transactions, with real-time updates and clear, detailed information.</li>
+  
+              <li><span className="font-bold">6.</span> 24/7 Customer Support: Have a question or need assistance? Our dedicated support team is available round-the-clock to address your queries and ensure a smooth experience on our platform.</li>
+  
+            </ul>
+  
+            <p className="mt-5">
+              
+              Join the Future of Crypto Swapping Today!
+              
+              Don't let outdated exchanges hold you back. Embrace the power of universal crosschain swaps and safeguard your assets with CryptoSwapPro. Join our ever-growing community of crypto enthusiasts and experience a new era of trading possibilities.
+              
+              Ready to take the leap? Sign up now and unlock the potential of universal crosschain swaps like never before!
+            </p>
+          </div>
+        </main>
+      </Layout>
+    )
+}
--- frontend/src/app/components/Nav.tsx
+++ frontend/src/app/components/Nav.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link"
 
 export default function Nav() {
     return( 
-        <div className='flex justify-center align-center bg-slate-800 p-2'>
+        <div className='flex justify-center align-center bg-slate-800 p-2 pr-4 pl-4'>
             <ul className='flex gap-2 text-[white]'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Chart</li>
+                <li><Link href='/'>Home</Link></li>
+                <li><Link href='about'>About</Link></li>
+                <li><a href='https://dexscreener.com/'>Chart</a></li>
             </ul>
-            <h1 className='text-[white] text-2xl ml-auto mr-auto'>My Shitcoin Website</h1>
-            <button className='text-[white]'>Buy Now</button>
+            <h1 className='text-[white] text-2xl ml-auto mr-auto whitespace-nowrap'>My Shitcoin Website</h1>
+            <button className='text-[white]'><a href="https://uniswap.org">Buy Now</a></button>
         </div>
     )
 }
